a9b14243313ac81863c28226cbb3c5c341d74a6a
Woulfe <pejovskitoni@yahoo.com>
2020-03-24 18:02:41 -0600

    Sync with class code

--- src/test/java/com/automation/tests/day10/ActionsTests.java
+++ src/test/java/com/automation/tests/day10/ActionsTests.java
@@ -11,6 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 public class ActionsTests {
     private WebDriver driver;
     private Actions actions;
