7410e234673fc22526c4e9c844c5159fea4d910d
Woulfe <pejovskitoni@yahoo.com>
2020-03-29 13:17:44 -0600

    new commit

--- src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstSeleniumScript {
 //test commit
+    //test commit
 
     public static void main(String[] args) {
 // 1st. setup chromedriver
